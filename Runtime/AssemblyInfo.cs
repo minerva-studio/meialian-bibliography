f0402229d05ee1729cb154dd05edeec0970beb13
Chad-Vine-Doll <164040662@qq.com>
2025-11-19 20:27:09 -0500

    Update AssemblyInfo.cs

--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -1,4 +1,22 @@
+using Minerva.DataStorage;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Minerva.DataStorage.Editor")]
 [assembly: InternalsVisibleTo("Minerva.DataStorage.Tests")]
+[assembly: AssemblyVersion(AssemblyInfo.Version)]
+[assembly: AssemblyFileVersion(AssemblyInfo.FileVersion)]
+
+namespace Minerva.DataStorage
+{
+    public class AssemblyInfo
+    {
+        /// <summary>
+        /// The game revision <br/>
+        /// Only need to change when want to publish a same version name but with changes on save/load
+        /// </summary> 
+        public const string VisionRevision = "0";
+        public const string Version = "0.1.0";
+        public const string FileVersion = Version + "." + VisionRevision;
+    }
+}
